08610ad1aa2fd855ba84937f2fd57637cd17d04d
Luna78453 <94145855+Luna78453@users.noreply.github.com>
2024-02-08 20:49:15 -0600

    creacion de algunos objetos

---
+++ ionic-basic/src/app/presupuesto/presupuesto.page.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-presupuesto',
+  templateUrl: './presupuesto.page.html'
+})
+export class PresupuestoPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
